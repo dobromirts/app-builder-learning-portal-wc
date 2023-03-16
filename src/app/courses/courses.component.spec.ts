2d2fd5dfaad7dfcbb71b9aa964b500bd686fbcf7
dobromirts <46093564+dobromirts@users.noreply.github.com>
2023-03-16 15:57:02 +0200

    Learning Portal initial version

---
+++ src/app/courses/courses.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { IgxListModule, IgcFormsModule, IgxChipsModule, IgxChipsModule, IgcFormsModule, IgxChipsModule, IgxChipsModule, IgxAvatarModule, IgxIconModule, IgxIconModule, IgxAccordionModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule } from '@infragistics/igniteui-angular';
+import { CoursesComponent } from './courses.component';
+
+describe('CoursesComponent', () => {
+  let component: CoursesComponent;
+  let fixture: ComponentFixture<CoursesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CoursesComponent ],
+      imports: [ NoopAnimationsModule, FormsModule, HttpClientTestingModule, IgxListModule, IgcFormsModule, IgxChipsModule, IgxChipsModule, IgcFormsModule, IgxChipsModule, IgxChipsModule, IgxAvatarModule, IgxIconModule, IgxIconModule, IgxAccordionModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxExpansionPanelModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule, IgxAvatarModule, IgxIconModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CoursesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
